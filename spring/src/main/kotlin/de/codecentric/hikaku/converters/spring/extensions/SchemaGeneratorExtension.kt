f32277f516ecf6c6951d6f68b91bac6557b516e5
Christian Sciberras <christian.sciberras@personio.de>
2019-07-11 18:24:18 +0200

    Fixes and annotaion basics

--- spring/src/main/kotlin/de/codecentric/hikaku/converters/spring/extensions/SchemaGeneratorExtension.kt
+++ spring/src/main/kotlin/de/codecentric/hikaku/converters/spring/extensions/SchemaGeneratorExtension.kt
@@ -3,30 +3,40 @@ package de.codecentric.hikaku.converters.spring.extensions
 import de.codecentric.hikaku.endpoints.schemas.*
 import kotlin.reflect.KClass
 import kotlin.reflect.KParameter
+import kotlin.reflect.KTypeProjection
 import kotlin.reflect.full.isSubclassOf
 import kotlin.reflect.full.memberProperties
 import kotlin.reflect.jvm.jvmErasure
 
-
 internal fun KParameter.toSchema(): SchemaInterface? =
-    this.type.jvmErasure.toSchema(this.type.arguments.map { it.type?.jvmErasure?.toSchema() })
+    this.type.jvmErasure.toSchema(
+        this.annotations,
+        this.type.arguments.map { it.toSchema() }
+    )
 
-internal fun KClass<*>.toSchema(variantArgs: List<SchemaInterface?>? = null): SchemaInterface? {
+internal fun KTypeProjection.toSchema(): SchemaInterface? =
+    this.type?.jvmErasure?.toSchema(
+        this.type?.annotations.orEmpty(),
+        this.type?.arguments?.map { it.toSchema() }
+    )
+
+internal fun KClass<*>.toSchema(annotations: List<Annotation>, variantArgs: List<SchemaInterface?>?): SchemaInterface? {
     return when {
         this.java == Boolean::class.java ->
             BoolSchema()
         this.java == Int::class.java ->
-            IntegerSchema(null, null)
+            IntegerSchema(annotations.min(), annotations.max())
         this.java == Float::class.java ->
-            FloatSchema(null, null)
+            FloatSchema(annotations.min(), annotations.max())
         this.java == String::class.java ->
-            StringSchema(null, null)
+            StringSchema(annotations.min(), annotations.max())
         this.isData ->
             ObjectSchema(
                 this.memberProperties
                     .map { prop ->
                         prop.name to prop.returnType.jvmErasure.toSchema(
-                            prop.returnType.arguments.mapNotNull { type -> type.type?.jvmErasure?.toSchema() }
+                            prop.annotations,
+                            prop.returnType.arguments.mapNotNull { type -> type.toSchema() }
                         )
                     }
                     .filter {
@@ -42,10 +52,14 @@ internal fun KClass<*>.toSchema(variantArgs: List<SchemaInterface?>? = null): Sc
                 checkNotNull(variantArgs?.getOrNull(0)) {
                     "Could not determine type of array"
                 },
-                null,
-                null
+                annotations.min(),
+                annotations.max()
             )
         else ->
             null
     }
 }
+
+internal fun <T> List<Annotation>.min(): T? = null // TODO this.firstOrNull{ it.annotationClass.qualifiedName == "javax.validation.constraints.Min" }
+
+internal fun <T> List<Annotation>.max(): T? = null // TODO
